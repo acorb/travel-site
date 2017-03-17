752aca6ee792ae1b5d5588a6782d4b83afab3eec
Anne <contact@acorbierre.com>
2017-03-17 21:04:28 +0100

    Completed automatic sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 require('./gulp/tasks/styles'); /* pas besoin d'indiquer l'extension .js */
 require('./gulp/tasks/watch'); /* pas besoin d'indiquer l'extension .js */
+require('./gulp/tasks/sprites'); /* pas besoin d'indiquer l'extension .js */
 
 	
 
